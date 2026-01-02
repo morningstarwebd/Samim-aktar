e056220faefe1f936cb1a4375f371902e6e30e2e
Samim Aktar <samimaktar081@gmail.com>
2026-01-02 20:35:40 +0000

    02:02:32.868 Running build in Washington, D.C., USA (East) – iad1 02:02:

--- src/app/api/genkit/[...slug]/route.ts
+++ src/app/api/genkit/[...slug]/route.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import {createApiHandler} from 'genkit';
+import {createApiHandler} from 'genkit/next';
 import '@/ai/flows/contact-info-suggestion';
 
 export const {GET, POST} = createApiHandler();
